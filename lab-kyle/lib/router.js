0f5a6e81a2aa504018d1d422a9185433c8cf8ff8
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-14 17:49:52 -0700

    follow along code

--- lab-kyle/lib/router.js
+++ lab-kyle/lib/router.js
@@ -1,12 +1,56 @@
 'use strict';
 
-let router = module.exports = exports = {};
-router.routes = {};
-let methods = ['get', 'post', 'put', 'patch', 'delete'];
+// let router = module.exports = exports = {};
+// router.routes = {};
+// let methods = ['get', 'post', 'put', 'patch', 'delete'];
+//
+// methods.forEach((method) => {
+//   router.routes[method.toUpperCase()] = {};
+//   router[method] = function(pathname, callback){
+//     router.routes[method.toUpperCase()][pathname] = callback;
+//   };
+// });
 
-methods.forEach((method) => {
-  router.routes[method.toUpperCase()] = {};
-  router[method] = function(pathname, callback){
-    router.routes[method.toUpperCase()][pathname] = callback;
-  };
-});
+const routeHandlers = {
+  GET: {},
+  POST: {},
+  PUT: {},
+  PATCH: {},
+  DELETE: {},
+};
+
+module.exports = {
+  get: (uri, callback) => {
+    routeHandlers.GET[uri] = callback;
+  },
+  post: (uri, callback) => {
+    routeHandlers.POST[uri] = callback;
+  },
+  put: (uri, callback) => {
+    routeHandlers.PUT[uri] = callback;
+  },
+  patch: (uri, callback) => {
+    routeHandlers.PATCH[uri] = callback;
+  },
+  delete: (uri, callback) => {
+    routeHandlers.DELETE[uri] = callback;
+  },
+  route: (req, res) => {
+    parser(req)
+      .then((req) => {
+        let handler = routeHandlers[req.method][req.url.pathname];
+        if (handler) {
+          return handler(req,res);
+        } else {
+          console.error('Not Found', req.url.pathname);
+          res.writeHead(404);
+          res.end();
+        }
+      })
+      .catch((err) => {
+        console.error('Invalid Request', err);
+        res.writeHead(400);
+        res.end();
+      });
+  },
+};
--- lab-kyle/lib/server.js
+++ lab-kyle/lib/server.js
@@ -4,15 +4,48 @@ const http = require('http');
 const url = require('url');
 const router = require('./lib/router.js');
 
-router.get('/', function(req,res){
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World');
-  res.end();
-})
+let isRunning = false;
 
-const server = http.createServer(function(req, res){
-  req.url = url.parse(req.url);
-  router.routes[req.method][req.url.pathname](req,res);
-});
+const app = http.createServer(router.route);
 
-server.listen(3000);
+module.exports = {
+  start: () => {
+
+    return new Promise((resolve, reject) => {
+      if (!isRunning){
+        app.listen(process.env.PORT, (err) => {
+          if (err){
+            reject(err);
+          } else {
+            isRunning = true;
+            resolve(`Server up on port ${process.env.PORT}`);
+          }
+        });
+      } else {
+        reject('Server is already runnning');
+      }
+    });
+  },
+
+  stop: () => {
+    return new Promise((resolve, reject) => {
+      if (!isRunning) {
+        
+      }
+    });
+  },
+
+};
+
+// router.get('/', function(req,res){
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('Hello World');
+//   res.end();
+// })
+//
+// const server = http.createServer(function(req, res){
+//   req.url = url.parse(req.url);
+//   router.routes[req.method][req.url.pathname](req,res);
+// });
+//
+// server.listen(3000);
