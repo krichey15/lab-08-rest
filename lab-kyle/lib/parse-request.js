b5c346f77e0ed52f6344fe27cdf56d30e754ad11
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-17 14:02:11 -0700

    finished parser

---
+++ lab-kyle/lib/parse-request.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const url = require('url');
+const queryString = require('query-string');
+
+module.exports = (req) = {
+
+  return new Promise((resolve, reject) => {
+
+    req.url = url.parse(req.url);
+    req.url.query = queryString.parse(req.url.query);
+
+    if (!(req.method === 'PUT' || req.method === 'POST' || req.method === 'PATCH')){
+      resolve(req);
+    }
+
+    let text = "";
+
+    req.on('data', (buffer) => {
+      text += buffer.toString();
+    });
+
+    req.on('end', () => {
+      try {
+        req.body = JSON.parse(text);
+      } catch (err){
+        console.log(err);
+        reject(err);
+      }
+    });
+    req.on('error', reject);
+  });
+};
--- lab-kyle/lib/router.js
+++ lab-kyle/lib/router.js
@@ -1,15 +1,6 @@
 'use strict';
 
-// let router = module.exports = exports = {};
-// router.routes = {};
-// let methods = ['get', 'post', 'put', 'patch', 'delete'];
-//
-// methods.forEach((method) => {
-//   router.routes[method.toUpperCase()] = {};
-//   router[method] = function(pathname, callback){
-//     router.routes[method.toUpperCase()][pathname] = callback;
-//   };
-// });
+const parser = require('./lib/parse-request.js');
 
 const routeHandlers = {
   GET: {},
